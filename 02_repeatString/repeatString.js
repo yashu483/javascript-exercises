48dd678d86f5132de495e75d935f3b3c25c395f4
Yashu Lilhare <182187926+yashu483@users.noreply.github.com>
2024-11-05 14:29:38 +0530

    02_repeatString exercise completed

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,4 +1,16 @@
-const repeatString = function() {
+
+const repeatString = function (string, num) {
+    if (num < 0) {
+        return "ERROR"
+    }
+    else if (num === undefined) {
+        num = Math.floor(Math.random() * 100);
+    }
+    let str = "";
+    for (let i = 1; i <= num; i++) {
+            str += string;
+    }
+    return str;
 
 };
 
