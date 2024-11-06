f5855ca9dcd223973dc0775ecf5956181f5e0015
Yashu Lilhare <182187926+yashu483@users.noreply.github.com>
2024-11-06 20:08:21 +0530

    complete 08_calculater exercise

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,26 +1,43 @@
-const add = function() {
-	
+const add = function (firstNum, secondNum) {
+  return firstNum + secondNum;
 };
 
-const subtract = function() {
-	
+const subtract = function (firstNum, secondNum) {
+  return firstNum - secondNum;
 };
 
-const sum = function() {
-	
+const sum = function (array) {
+  let result = 0;
+  array.map((value) => result += value);
+  return result;
 };
 
-const multiply = function() {
-
+const multiply = function (array) {
+  let result = 1
+  array.map((value) => result *= value);
+  return result;
 };
 
-const power = function() {
-	
+const power = function (base, power) {
+  let result = base;
+  for (let i = 1; i < power; i++) {
+    result *= base;
+  }
+  return result;
 };
 
-const factorial = function() {
-	
-};
+const factorial = function (number) {
+  let result;
+  if (number === 0) {
+    return 1;
+  }
+  else {
+    for (let i = number - 1; i >= 1; i--) {
+      number = number * i ;
+    }
+    return number ;
+  };
+}
 
 // Do not edit below this line
 module.exports = {
