695ebda89c5a8060697fdf12bf47549b474699a8
Yashu Lilhare <182187926+yashu483@users.noreply.github.com>
2024-11-06 16:02:04 +0530

    Complete 05_sumAll exercise

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,32 @@
-const sumAll = function() {
+const sumAll = function (num1, num2) {
+    let smallNum;
+    let bigNum;
+    let total = 0;
 
+    if (num1 < num2) {
+        smallNum = num1;
+        bigNum = num2;
+    }
+    else if (num1 > num2) {
+        smallNum = num2;
+        bigNum = num1;
+    };
+
+    if (smallNum < 0 || bigNum < 0) {
+        return "ERROR"
+    }
+    else if (!Number.isInteger(smallNum) || !Number.isInteger(bigNum)) {
+        return "ERROR"
+    }
+    else if (typeof smallNum !== "number" || typeof bigNum !== "number") {
+        return "ERROR"
+    }
+    else {
+        for (let i = 1; i <= bigNum; i++) {
+            total += i;
+        }
+        return total;
+    }
 };
 
 // Do not edit below this line
