491d4df5e9dc6e56a742780b9ddfa11f2ca10b3f
Yashu Lilhare <182187926+yashu483@users.noreply.github.com>
2024-11-06 22:13:27 +0530

    complete 10_fibonacci exercises

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,19 @@
-const fibonacci = function() {
+const fibonacci = function (position) {
+    let array = [0, 1];
 
+    if (typeof position === "string") {
+        +position;
+    } ;
+    if (position == 0) { return 0 } ;
+
+    if (position < 0) {
+        return "OOPS";
+    } ;
+    for (let i = 1; i < position; i++) {
+        array.push((array[(array.length - 2)] + array[array.length - 1]));
+
+    };
+    return +array[array.length - 1];
 };
 
 // Do not edit below this line
