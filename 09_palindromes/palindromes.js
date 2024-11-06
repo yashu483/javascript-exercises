14fcc24e023b98b45648a127d79e229e22020785
Yashu Lilhare <182187926+yashu483@users.noreply.github.com>
2024-11-06 21:19:59 +0530

    complete 09_palindrones exercises

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,13 @@
-const palindromes = function () {
+const palindromes = function (string) {
 
+const onlyLetters = string.replace( /[\W_]+/g , "").toLowerCase() ;
+
+let reversedString = onlyLetters ;
+let reversedStringNew = reversedString.split("").reverse().join("").toLowerCase() ;
+
+return onlyLetters === reversedStringNew ;
 };
+
 
 // Do not edit below this line
 module.exports = palindromes;
