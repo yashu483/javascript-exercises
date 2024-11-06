a1dda8aebe93c0d1ca08f6bd2dee5c90adff3d7e
Yashu Lilhare <182187926+yashu483@users.noreply.github.com>
2024-11-06 19:01:20 +0530

    complete 07_tempConversion exercise

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,13 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(fahrenheit) {
+  let convertedValue = (fahrenheit - 32) * ( 5 / 9) ;
+  let result = Math.round(convertedValue * 10) / 10 ;
+return result ;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(celsius) { 
+  let convertedValue = (celsius * (9/5)) + 32 ;
+  let result = Math.round(convertedValue * 10) / 10 ;
+  return result ;
 };
 
 // Do not edit below this line
