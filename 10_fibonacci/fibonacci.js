feef8a0af9c782e5841852a8d74310327077d8f0
Yashu Lilhare <182187926+yashu483@users.noreply.github.com>
2024-12-13 14:28:04 +0530

    completed 10 fibbonaci sequence exercises

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,18 @@
-const fibonacci = function() {
+const fibonacci = function (num) {
+    if (typeof num === 'string') (+num);
+    if (num < 0) return `OOPS`;
+    if (num == 0) return 0;
 
+    let prev = 0;
+    let next = 1;
+    let total = 0;
+
+    for (i = 2; i <= num; i++) {
+        total = prev + next;
+        prev = next;
+        next = total;
+    }
+    return next;
 };
 
 // Do not edit below this line
