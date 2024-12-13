70b13f2d8e6e8a25b5843119d60dcbe1754bfe24
Yashu Lilhare <182187926+yashu483@users.noreply.github.com>
2024-12-13 16:36:38 +0530

    completed 12 getAge exercises

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,16 @@
-const findTheOldest = function() {
+const findTheOldest = function (arr) {
+    arr.map((obj) => {
+        if (obj.yearOfDeath === undefined)
+            obj.yearOfDeath = (new Date()).getFullYear();
+    });
+    let sortOldest = arr.sort((a, b) => {
+        const ageA = a.yearOfDeath - a.yearOfBirth;
+        const ageB = b.yearOfDeath - b.yearOfBirth;
 
+        return ageB - ageA;
+    });
+
+    return sortOldest[0];
 };
 
 // Do not edit below this line
