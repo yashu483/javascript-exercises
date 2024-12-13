5785cc08d91f8843972db5ec7d67bb3f4f7ae26e
Yashu Lilhare <182187926+yashu483@users.noreply.github.com>
2024-12-13 10:33:42 +0530

    complete 03 reverse string exercises

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,10 @@
-const reverseString = function() {
+const reverseString = function (string) {
+    const str = string
+        .split('')
+        .reverse()
+        .join('');
 
+    return str;
 };
 
 // Do not edit below this line
