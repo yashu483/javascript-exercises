c49e4ecd06972ebd97a3c8b43527c940af3ecb85
Yashu Lilhare <182187926+yashu483@users.noreply.github.com>
2024-12-13 13:46:17 +0530

    completed 08 factorial exercises

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,43 @@
-const add = function() {
-	
+const add = function (num1, num2) {
+  return num1 + num2;
 };
 
-const subtract = function() {
-	
+const subtract = function (num1, num2) {
+  return num1 - num2;
 };
 
-const sum = function() {
-	
+const sum = function (arr) {
+  return arr.reduce((total, num) => {
+    total += num;
+    return total;
+  }, 0)
+
 };
 
-const multiply = function() {
+const multiply = function (arr) {
+  return arr.reduce((total, num) => {
+    total *= num;
+    return total;
+  }, 1);
 
 };
 
-const power = function() {
-	
+const power = function (num1, num2) {
+  let sum = 1;
+  for (i = 0; i < num2; i++) {
+    sum *= num1;
+  }
+  return sum;
 };
 
-const factorial = function() {
-	
+const factorial = function (num) {
+  if (num === 0) return 1;
+
+  let total = 1;
+  for (i = num; i > 0; i--) {
+    total *= i;
+  }
+  return total;
 };
 
 // Do not edit below this line
