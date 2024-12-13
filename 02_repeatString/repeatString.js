81945a7f5283267b21a1cb25e041577315f00b8c
Yashu Lilhare <182187926+yashu483@users.noreply.github.com>
2024-12-13 10:29:08 +0530

    completed 02 repeatsting exercises

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,12 @@
-const repeatString = function() {
-
+const repeatString = function (string, num) {
+    if (num < 0) {
+        return 'ERROR';
+    }
+    let str = '';
+    for (i = 0; i < num; i++) {
+        str += string
+    };
+    return str;
 };
 
 // Do not edit below this line
