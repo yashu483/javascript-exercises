a22bb159f0690f0227c568fe2a462f0432406b07
Yashu Lilhare <182187926+yashu483@users.noreply.github.com>
2024-12-13 16:14:00 +0530

    completed 09 palindromes exercises

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,4 +1,16 @@
-const palindromes = function () {
+const palindromes = function (string) {
+    let arr = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '0', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n',
+        'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'
+    ]
+    let lowered = string.toLowerCase();
+
+    let filtered = lowered.split('').filter((char) => {
+        if (arr.includes(char)) return true;
+    }).join('');
+
+    let reversed = filtered.split('').reverse().join('');
+
+    return filtered == reversed;
 
 };
 
